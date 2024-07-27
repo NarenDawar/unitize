694b684bf278a69dea3056f08cbb746d571f06c0
NarenDaw <narendaw1@gmail.com>
2024-07-26 23:00:39 -0400

    Updated utils.

--- unitize/util.py
+++ unitize/util.py
@@ -3,6 +3,11 @@ from .mass import MassConverter
 from .temperature import TemperatureConverter
 from .volume import VolumeConverter
 from .time import TimeConverter
+from .data_storage import DataStorageConverter
+from .speed import SpeedConverter
+from .pressure import PressureConverter
+from .energy import EnergyConverter
+from .frequency import FrequencyConverter
 
 def convert(value, from_unit, to_unit, category):
     if category == 'length':
@@ -15,5 +20,15 @@ def convert(value, from_unit, to_unit, category):
         return VolumeConverter.convert(value, from_unit, to_unit)
     elif category == 'time':
         return TimeConverter.convert(value, from_unit, to_unit)
+    elif category == 'data_storage':
+        return DataStorageConverter.convert(value, from_unit, to_unit)
+    elif category == 'speed':
+        return SpeedConverter.convert(value, from_unit, to_unit)
+    elif category == 'pressure':
+        return PressureConverter.convert(value, from_unit, to_unit)
+    elif category == 'energy':
+        return EnergyConverter.convert(value, from_unit, to_unit)
+    elif category == 'frequency':
+        return FrequencyConverter.convert(value, from_unit, to_unit)
     else:
-        raise ValueError("Invalid category for conversion. Check documentation if confused.")
+        raise ValueError("Invalid category for conversion. Check documentation.")
